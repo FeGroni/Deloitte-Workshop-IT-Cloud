e109521f2d14ca3197543618b5500529f49da81f
fegroni <felix.gronwald@gmail.com>
2023-06-02 11:03:33 +0200

    xy

--- treatment.py
+++ treatment.py
@@ -1,30 +1,17 @@
 import pandas as pd
 
-class Address:
-    def __init__(self, state, city, street, housenumber, zipcode, referralregion):
-        self.state = state
-        self.city = city
-        self.street = street
-        self.housenumber = housenumber
-        self.zipcode = zipcode
-        self.referralregion = referralregion
-
+class Treatment:
+    def __init__(self, name, numberoftreats, avgcovered, avgtotalpay, avgmedicalpay):
+        self.name = name
+        self.numberoftreats = numberoftreats
+        self.avgcovered = avgcovered
+        self.avgtotalpay = avgtotalpay
+        self.avgmedicalpay = avgmedicalpay
+        
     def __str__(self):
-        return f"Address: {self.housenumber} {self.street}, {self.city}, {self.state}, {self.zipcode}, {self.referralregion}"
+        return f"Address: {self.name} {self.numberoftreats}, {self.avgcovered}, {self.avgtotalpay}, {self.avgmedicalpay}"
 
-df = pd.read_csv('inpatientCharges.csv', sep=';', low_memory=False)
+df2 = pd.read_csv('inpatientCharges.csv', sep=';', low_memory=False)
 
-addresses = []
-for index, row in df.iterrows():
-    state = row['Provider State']
-    city = row['Provider City']
-    street = row['Provider Street Address']
-    housenumber = row['Provider Id']
-    zipcode = row['Provider Zip Code']
-    referralregion = row['Hospital Referral Region Description']
+treatments = []
 
-    address = Address(state, city, street, housenumber, zipcode, referralregion)
-    addresses.append(address)
-
-for address in addresses:
-    print(address)
